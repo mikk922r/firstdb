2c429562f98c56f5c93d1abaa993acf2f9f1c241
mikk922r <mikk922r@edu.mercantec.dk>
2025-04-22 11:28:29 +0200

    Navn på kategori tabel ændret og data til kategorier

--- DBSchema/Categories.sql
+++ DBSchema/Categories.sql
@@ -1,5 +1,11 @@
 -- @block Lav tabel til kategorier
-CREATE TABLE category (
+CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     name TEXT
 );
+
+-- @block Indsæt kategorier
+INSERT INTO categories VALUES 
+(1, 'Trøjer'),
+(2, 'Bukser'),
+(3, 'Sko')
--- DBSchema/Products.sql
+++ DBSchema/Products.sql
@@ -12,6 +12,6 @@ CREATE TABLE products (
     category_id INTEGER NOT NULL,
     user_id INTEGER,
     FOREIGN KEY (brand_id) REFERENCES brands(id),
-    FOREIGN KEY (category_id) REFERENCES category(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
