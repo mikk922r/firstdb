34cf85dcd0c24af49affc0bc8478ccc824555ae1
mikk922r <mikk922r@edu.mercantec.dk>
2025-04-22 12:20:17 +0200

    fjern unødvendigt sql

--- DBSchema/Users.sql
+++ DBSchema/Users.sql
@@ -11,11 +11,4 @@ CREATE TABLE users (
 INSERT INTO users VALUES 
 (1, 'Oliver', 'oliveragervig@gmail.com', 45, '29705200'),
 (2, 'Mikkel', 'mikk922r@edu.mercantec.dk', 45, '25772277'),
-(3, 'Nicolai', 'nicolaivikkelsoe@gmail.com', NULL, NULL),
-(4, 'test', 'test', NULL, NULL);
-
--- @block Opdater Nicolai
-UPDATE users SET phone_code = 45, phone_number = '42502874' WHERE id = 3;
-
--- @block Slet test bruger
-DELETE FROM users WHERE phone_code IS NULL AND phone_number IS NULL;
+(3, 'Nicolai', 'nicolaivikkelsoe@gmail.com', 45, '42502874');
