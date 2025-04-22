99382939c03a7736d660635fada9011fe1102f65
mikk922r <mikk922r@edu.mercantec.dk>
2025-04-22 11:41:49 +0200

    Flere ting til users tabellen

--- DBSchema/Users.sql
+++ DBSchema/Users.sql
@@ -6,9 +6,16 @@ CREATE TABLE users (
     phone_code INTEGER,
     phone_number TEXT
 );
--- @block 
-INSERT INTO users (name, email, phone_code, phone_number) VALUES ('Oliver', 'oliveragervig@gmail.com', 45, '29705200');
 
-SELECT * FROM users;
+-- @block Indsæt brugere
+INSERT INTO users VALUES 
+(1, 'Oliver', 'oliveragervig@gmail.com', 45, '29705200'),
+(2, 'Mikkel', 'mikk922r@edu.mercantec.dk', 45, '25772277'),
+(3, 'Nicolai', 'nicolaivikkelsoe@gmail.com', NULL, NULL),
+(4, 'test', 'test', NULL, NULL);
 
+-- @block Opdater Nicolai
+UPDATE users SET phone_code = 45, phone_number = '42502874' WHERE id = 3;
 
+-- @block Slet test bruger
+DELETE FROM users WHERE phone_code IS NULL AND phone_number IS NULL;
