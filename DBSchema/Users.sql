e8339deedb39a927e4f6e1a239379842c22b5a6b
OliverAgB <oliveragervig@gmail.com>
2025-04-22 11:34:56 +0200

    New

--- DBSchema/Users.sql
+++ DBSchema/Users.sql
@@ -6,3 +6,9 @@ CREATE TABLE users (
     phone_code INTEGER,
     phone_number TEXT
 );
+-- @block 
+INSERT INTO users (name, email, phone_code, phone_number) VALUES ('Oliver', 'oliveragervig@gmail.com', 45, '29705200');
+
+SELECT * FROM users;
+
+
