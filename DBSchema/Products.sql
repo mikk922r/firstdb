20b7e3ca69884875e259debb29e8870889f9612d
mikk922r <mikk922r@edu.mercantec.dk>
2025-04-22 12:31:07 +0200

    View til at få alle produkter med mærkenavn, kategorinavn og brugernavn

--- DBSchema/Products.sql
+++ DBSchema/Products.sql
@@ -15,3 +15,26 @@ CREATE TABLE products (
     FOREIGN KEY (category_id) REFERENCES categories(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+-- @block View til at få alle produkter med brandnavn, kategorinavn og brugernavn (hvis user_id er sat og brugeren findes)
+CREATE VIEW get_products_with_names AS 
+SELECT 
+    p.id, 
+    p.name,
+    p.description,
+    p.price,
+    p.color,
+    p.size,
+    p.quantity,
+    p.used,
+    b.name AS brand_name,
+    c.name AS category_name,
+    u.name AS user_name
+FROM 
+    products p
+    JOIN brands b ON b.id = p.brand_id
+    JOIN categories c ON c.id = p.category_id
+    LEFT JOIN users u ON u.id = p.user_id
+
+-- @block Test view
+SELECT * FROM get_products_with_names;
